3b29b3b85fa3628dd34db07432d39c7f5eed247c
pasyet <farapasyetnabila@gmail.com>
2018-01-04 00:45:32 +0700

    Latihan Sendiri

---
+++ IfJava.java
@@ -0,0 +1,11 @@
+public class IfJava {
+    public static void main (String[] args){
+        Integer bil1 = 10;
+        if (bil1 == 100){
+            System.out.println ("Bilangan Sama");
+        }
+        else {
+            System.out.println ("Bilangan Tidak Sama");
+        }
+    }
+}
---
+++ Swicth.java
@@ -0,0 +1,15 @@
+public class Swicth{
+    public static void main (String[] args){
+        Integer value = 100;
+        switch (value){
+            case 10:
+            System.out.println("Nilai sama dengan 10");
+            break;
+            case 20:
+            System.out.println("Nilai sama dengan 20");
+            break;
+            default : System.out.println("Nilai tidak ada yang sama ");
+        }
+        
+    }
+}
